380774956c59671a35fe704ec035404b24a6a2bc
TempusGPT <contact@changyong.me>
2024-04-17 22:35:15 +0900

    Fix going root when refresh bug

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { mount } from "svelte";
-import Router from "svelte-spa-router";
+import Router, { replace } from "svelte-spa-router";
 
-import EditorPage from "./pages/EditorPage.svelte";
-import ErrorPage from "./pages/ErrorPage.svelte";
-import HomePage from "./pages/HomePage.svelte";
-import LoginPage from "./pages/LoginPage.svelte";
-import SettingsPage from "./pages/SettingsPage.svelte";
+import EditorPage from "~/pages/EditorPage.svelte";
+import ErrorPage from "~/pages/ErrorPage.svelte";
+import HomePage from "~/pages/HomePage.svelte";
+import LoginPage from "~/pages/LoginPage.svelte";
+import SettingsPage from "~/pages/SettingsPage.svelte";
 
 const routes = {
     "/": HomePage,
@@ -15,7 +15,10 @@ const routes = {
     "/*": ErrorPage,
 };
 
-window.location.hash = "/";
+if (!window.location.hash) {
+    replace("/");
+}
+
 mount(Router, {
     target: document.getElementById("root")!,
     props: { routes },
