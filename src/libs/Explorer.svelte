5231d1469eea513bf2601da567a495601830d948
TempusGPT <contact@changyong.me>
2024-04-17 20:00:42 +0900

    Make explorer component

---
+++ src/libs/Explorer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts" context="module">
+    const MOCKUP = [
+        { id: 0, path: "/Some Note" },
+        { id: 1, path: "/Other Note" },
+        { id: 2, path: "/Folder/Note in Folder" },
+    ] as const;
+</script>
+
+<div>
+    <h2>Explorer</h2>
+    <ul>
+        {#each MOCKUP as { id, path }}
+            <li>{id}: {path}</li>
+        {/each}
+    </ul>
+</div>
--- src/pages/Explorer.svelte
+++ src/pages/Explorer.svelte
@@ -1,1 +1,10 @@
-<div>Explorer</div>
+<script>
+    import Explorer from "~/libs/Explorer.svelte";
+</script>
+
+<main class="container-fluid">
+    <div class="grid">
+        <Explorer />
+        <article>Some content</article>
+    </div>
+</main>
