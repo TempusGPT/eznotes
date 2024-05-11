5ecf04414961730f48df0e510835416deeafe412
TempusGPT <contact@changyong.me>
2024-05-11 20:06:53 +0900

    Add withdraw method to server.auth

--- src/libs/server/auth.svelte.ts
+++ src/libs/server/auth.svelte.ts
@@ -4,7 +4,15 @@ export const auth = {
     async signIn() {
         await supabase.auth.signInWithOAuth({ provider: "google" });
     },
+
     async signOut() {
         await supabase.auth.signOut();
+    },
+
+    async withdraw() {
+        const res = await supabase.auth.getUser();
+        if (res.data.user) {
+            await supabase.auth.admin.deleteUser(res.data.user.id);
+        }
     },
 } as const;
