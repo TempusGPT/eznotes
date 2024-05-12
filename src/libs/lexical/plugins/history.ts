1c1468ed6860db1dca10a98b20e081876813c64c
TempusGPT <contact@changyong.me>
2024-05-12 21:03:09 +0900

    Refactor and formatting

--- src/libs/lexical/plugins/history.ts
+++ src/libs/lexical/plugins/history.ts
@@ -1,9 +1,9 @@
 import { createEmptyHistoryState, registerHistory } from "@lexical/history";
 import type { Plugin } from "./types";
 
-export const historyPlugin = (delay: number): Plugin => ({
+export const historyPlugin = (delay: number, state = createEmptyHistoryState()): Plugin => ({
     nodes: [],
     register(editor) {
-        return [registerHistory(editor, createEmptyHistoryState(), delay)];
+        return [registerHistory(editor, state, delay)];
     },
 });
--- src/libs/lexical/plugins/link.ts
+++ src/libs/lexical/plugins/link.ts
@@ -1,19 +1,27 @@
-import { CLICK_COMMAND, COMMAND_PRIORITY_LOW, type CommandListener } from "lexical";
+import { CLICK_COMMAND, COMMAND_PRIORITY_LOW } from "lexical";
 import { LinkNode } from "@lexical/link";
 import type { Plugin } from "./types";
 
 export const linkPlugin = (): Plugin => ({
     nodes: [LinkNode],
+
     register(editor) {
-        return [editor.registerCommand(CLICK_COMMAND, handleLinkClick, COMMAND_PRIORITY_LOW)];
+        return [
+            editor.registerCommand(
+                CLICK_COMMAND,
+                ({ target }) => {
+                    if (
+                        target instanceof HTMLElement &&
+                        target.parentElement instanceof HTMLAnchorElement
+                    ) {
+                        window.location.href = target.parentElement.href;
+                        return true;
+                    }
+
+                    return false;
+                },
+                COMMAND_PRIORITY_LOW,
+            ),
+        ];
     },
 });
-
-const handleLinkClick: CommandListener<MouseEvent> = ({ target }) => {
-    if (target instanceof HTMLElement && target.parentElement instanceof HTMLAnchorElement) {
-        window.location.href = target.parentElement.href;
-        return true;
-    }
-
-    return false;
-};
