10245cd3c07c92d3f75ea8e09970b26380dd9895
TempusGPT <contact@changyong.me>
2024-05-12 21:00:13 +0900

    Make link clickable

--- src/libs/lexical/plugins/link.ts
+++ src/libs/lexical/plugins/link.ts
@@ -1,9 +1,19 @@
+import { CLICK_COMMAND, COMMAND_PRIORITY_LOW, type CommandListener } from "lexical";
 import { LinkNode } from "@lexical/link";
 import type { Plugin } from "./types";
 
 export const linkPlugin = (): Plugin => ({
     nodes: [LinkNode],
-    register() {
-        return [];
+    register(editor) {
+        return [editor.registerCommand(CLICK_COMMAND, handleLinkClick, COMMAND_PRIORITY_LOW)];
     },
 });
+
+const handleLinkClick: CommandListener<MouseEvent> = ({ target }) => {
+    if (target instanceof HTMLElement && target.parentElement instanceof HTMLAnchorElement) {
+        window.location.href = target.parentElement.href;
+        return true;
+    }
+
+    return false;
+};
